c682608f8309adab1df0101af5d5759b8d468222
nqdung04 <dung.nq.2224@aptechlearning.edu.vn>
2025-07-28 22:25:50 +0700

    update them

--- script/product_detail.js
+++ script/product_detail.js
@@ -2,19 +2,13 @@ document.getElementById("openOrderBtn").addEventListener("click", function () {
     document.getElementById("orderOverlay").style.display = "flex";
 });
 
-document.getElementById("phone").addEventListener("input", function () {
-    this.value = this.value.replace(/\D/g, ""); // Loại bỏ mọi ký tự không phải số
-});
-
 function closeOrderForm() {
     document.getElementById("orderOverlay").style.display = "none";
 }
 
 const overlay = document.getElementById("imageOverlay");
-const zoomed = document.getElementById("zoomedImage");
 
 document.getElementById("mainImage").addEventListener("click", function () {
-    zoomed.src = this.src;
     overlay.style.display = "flex";
 });
 
@@ -23,21 +17,4 @@ overlay.addEventListener("click", function (e) {
     if (e.target === overlay) {
         overlay.style.display = "none";
     }
-});
-
-function closeImage() {
-    overlay.style.display = "none";
-}
-
-// chuyển ảnh
-document.addEventListener("DOMContentLoaded", function () {
-  const thumbnails = document.querySelectorAll(".thumbnail");
-  const mainImage = document.getElementById("mainImage");
-
-  thumbnails.forEach(thumbnail => {
-    thumbnail.addEventListener("click", function () {
-      const newSrc = this.getAttribute("src");
-      mainImage.setAttribute("src", newSrc);
-    });
-  });
 });
